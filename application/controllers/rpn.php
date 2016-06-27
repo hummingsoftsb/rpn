bbd3ff40f59910e8150bce0dcf87abcba773f675
Admin <admin@hummingsoft.com.my>
2016-06-27 12:38:07 +0800

    add guzzle

--- application/controllers/rpn.php
+++ application/controllers/rpn.php
@@ -6,11 +6,40 @@ class Rpn extends CI_Controller {
 		parent::__construct();
 		$this->load->helper('url');
     $this->load->library('form_validation');
+		$this->load->library('guzzle');
 
 	}
 
 	public function index()
 	{
 		$this->load->view('form');
+	}
+
+	public function test(){
+		$client     = new GuzzleHttp\Client();
+
+	  #This url define speific Target for guzzle
+	  $url        = 'http://services.groupkt.com/state/get/IND/all';
+
+	  #guzzle
+	  try {
+	    # guzzle post request example with form parameter
+	    $response = $client->request( 'GET',
+	                                   $url,
+	                                  [ 'form_params'
+	                                        => [ 'processId' => '2' ]
+	                                  ]
+	                                );
+	    #guzzle repose for future use
+	    echo $response->getStatusCode(); // 200
+	    echo $response->getReasonPhrase(); // OK
+	    echo $response->getProtocolVersion(); // 1.1
+	    echo $response->getBody();
+	  } catch (GuzzleHttp\Exception\BadResponseException $e) {
+	    #guzzle repose for future use
+	    $response = $e->getResponse();
+	    $responseBodyAsString = $response->getBody()->getContents();
+	    print_r($responseBodyAsString);
+	  }
 	}
 }
---
+++ application/libraries/guzzle.php
@@ -0,0 +1,10 @@
+<?php
+  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+  class Guzzle {
+    public function Guzzle() {
+      // require_once('vendor/autoload.php');
+      require APPPATH . "/third_party/guzzle/autoload.php";
+    }
+  }
+?>
